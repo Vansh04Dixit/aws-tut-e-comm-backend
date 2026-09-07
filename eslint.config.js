0adb8bc679d76436a227d0e5e251caac6ea7df54
PRASANNA DIXIT <www.dixitprasanna4@gmail.com>
2026-09-07 12:27:22 +0530

    Solved <process> issue in CI/CD

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,12 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 ]);
